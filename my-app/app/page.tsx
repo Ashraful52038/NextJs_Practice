ea43c9a671b319ccf1cca26f1b45c4ef111880f7
Ashraful Islam <youremail@example.com>
2025-12-18 18:40:56 +0600

    Design button, and update tailwindconfig

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,13 +3,13 @@
 
 function Homepage() {
   return (
-    <div>
-      <button>
-        +
-      </button>
-      <button>
-        -
-      </button>
+    <div className="flex gap-6 justify-center mt-20">
+        <button className="w-24 h-24 bg-blue-500 hover:bg-blue-600 text-white text-5xl">
+          +
+        </button>
+        <button className="w-24 h-24 bg-red-500 hover:bg-red-600 text-white text-5xl">
+          -
+        </button>
     </div>
   )
 }
