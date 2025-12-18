83b877e23845554f75440d4a3c605295bf0cc109
Ashraful Islam <youremail@example.com>
2025-12-18 17:36:56 +0600

    created +, - button and update layout.tsx

---
+++ my-app/app/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+
+function Homepage() {
+  return (
+    <div>
+      <button>
+        +
+      </button>
+      <button>
+        -
+      </button>
+    </div>
+  )
+}
+
+export default Homepage;
