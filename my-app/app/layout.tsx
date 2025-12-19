00e5b3e4da3338e11cc2057909fe67270a8ced2c
Ashraful Islam <youremail@example.com>
2025-12-19 12:06:37 +0600

    update layout page for redux and create storeProvider page

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,13 +1,21 @@
-import CounterProvider from '@/context/Counter'
-import './globals.css'
-// app/layout.js
+import { Inter } from 'next/font/google';
+import './globals.css';
+const inter = Inter({ subsets: ['latin'] })
+
+import { Metadata } from "next";
+import StoreProvider from './providers/storeProvider';
+
+export const metadata : Metadata ={
+    title: "DeadPool Counter Redux toolkit",
+    description: 'Counter app using Redux Toolkit with Immer',
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <CounterProvider>
+      <body className={inter.className}>
+        <StoreProvider>
           {children}
-        </CounterProvider>
+        </StoreProvider>
       </body>
     </html>
   )
